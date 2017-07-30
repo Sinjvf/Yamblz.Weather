6c7d93412109bd610895e8372591c6aa5147a5ed
illarionovR <illroman@yandex.ru>
2017-07-30 22:40:54 +0300

    Add WeatherFragment (toolbar only) test. Add ui test suite.

---
+++ app/src/androidTest/java/ru/mobilization/sinjvf/yamblzweather/ui/UiTestSuite.java
@@ -0,0 +1,13 @@
+package ru.mobilization.sinjvf.yamblzweather.ui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by exwhythat on 30.07.17.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({NavigationTest.class, AboutFragmentTest.class, SettingsFragmentTest.class, WeatherFragmentTest.class})
+public class UiTestSuite {
+}
