c770e32ff5b3e8e924f95fb2deabb196ccb1cbb1
illarionovR <illroman@yandex.ru>
2017-08-13 23:30:01 +0300

    Update CityInfo class test

--- app/src/test/java/ru/exwhythat/yather/CityTest.java
+++ app/src/test/java/ru/exwhythat/yather/CityTest.java
@@ -2,7 +2,7 @@ package ru.exwhythat.yather;
 
 import org.junit.Test;
 
-import ru.exwhythat.yather.screens.settings.CityInfo;
+import ru.exwhythat.yather.data.local.entities.City;
 
 import static org.junit.Assert.*;
 
@@ -10,34 +10,34 @@ import static org.junit.Assert.*;
  * Created by exwhythat on 30.07.17.
  */
 
-public class CityInfoTest {
+public class CityTest {
 
     @Test
     public void testEquals() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", false);
         assertEquals(cityInfo1, cityInfo2);
     }
 
     @Test
     public void testEqualsReflexive() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
         assertEquals(cityInfo1, cityInfo1);
     }
 
     @Test
     public void testEqualsSymmetric() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", false);
         assertEquals(cityInfo1, cityInfo2);
         assertEquals(cityInfo2, cityInfo1);
     }
 
     @Test
     public void testEqualsTransitive() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo3 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", false);
+        City cityInfo3 = new City(55, "Moscow", false);
         assertEquals(cityInfo1, cityInfo2);
         assertEquals(cityInfo2, cityInfo3);
         assertEquals(cityInfo3, cityInfo1);
@@ -45,49 +45,49 @@ public class CityInfoTest {
 
     @Test
     public void testNotEqualsNull() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
         assertNotEquals(cityInfo1, null);
     }
 
     @Test
     public void testNotEquals_1() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow2", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow2", false);
         assertNotEquals(cityInfo1, cityInfo2);
     }
 
     @Test
     public void testNotEquals_2() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 51, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(51, "Moscow", false);
         assertNotEquals(cityInfo1, cityInfo2);
     }
 
     @Test
     public void testNotEquals_3() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 31);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", true);
         assertNotEquals(cityInfo1, cityInfo2);
     }
 
     @Test
     public void testHashCode_Equals() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", false);
         assertTrue(cityInfo1.hashCode() == cityInfo2.hashCode());
     }
 
     @Test
     public void testHashCode_Not_Equals_1() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow2", 55, 33);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow2", false);
         assertTrue(cityInfo1.hashCode() != cityInfo2.hashCode());
     }
 
     @Test
     public void testHashCode_Not_Equals_2() {
-        CityInfo cityInfo1 = new CityInfo("Moscow", 55, 33);
-        CityInfo cityInfo2 = new CityInfo("Moscow", 55, 31);
+        City cityInfo1 = new City(55, "Moscow", false);
+        City cityInfo2 = new City(55, "Moscow", true);
         assertTrue(cityInfo1.hashCode() != cityInfo2.hashCode());
     }
 }
